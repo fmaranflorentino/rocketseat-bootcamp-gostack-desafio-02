39e102a47e51f433eefc8be23293835837632376
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-02-09 20:22:26 -0200

    feat(*): Added prettier, eslint e editorconfig

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base', 'prettier'
+  ],
+  plugins: [ 'prettier' ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-params-reassign": "off",
+    "camelcase": "off",
+    "no-unsed-vars": ["error", { "argsIgnorePattern": "next" }]
+  },
+};
