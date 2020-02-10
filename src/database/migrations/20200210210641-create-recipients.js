31e68c05b7923eae67d8a7190ae582bbfb17e3e5
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-02-10 19:14:00 -0200

    feat(sequelize): Create recipents migration

---
+++ src/database/migrations/20200210210641-create-recipients.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('recipients', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNul: false,
+      },
+      number: {
+        type: Sequelize.INTEGER,
+        allowNul: false,
+      },
+      complement: {
+        type: Sequelize.STRING,
+        allowNul: false,
+      },
+      state: {
+        type: Sequelize.STRING,
+        allowNul: false,
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNul: false,
+      },
+      postalCode: {
+        type: Sequelize.STRING,
+        allowNul: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNul: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNul: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  },
+};
